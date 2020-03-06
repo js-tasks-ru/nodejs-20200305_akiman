0bbd2fc43fbb7cf31048ae25ed9268392b5917ca
AKIMOV Aleksey <aleksey.akimov@vaz.ru>
2020-03-06 14:30:01 +0400

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
   /* ваш код */
+    if(parseInt(a)>=0 && parseInt(b)>=0) {
+        return a + b;
+    } else {
+        throw new TypeError();
+    }
 }
 
 module.exports = sum;
