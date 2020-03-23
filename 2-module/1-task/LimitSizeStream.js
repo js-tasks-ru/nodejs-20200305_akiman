5eaf21d49172cd4576cb1cc8111950b44d9170aa
AKIMOV Aleksey <aleksey.akimov@vaz.ru>
2020-03-23 21:08:42 +0400

    решение задачи 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,24 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit; // из переданного объекта параметров вытаскиваем параметр 'limit' и сохран€ем в переменную класса
+    this.count = 0;
+  }
+
+  getBinarySize(chunk, encoding) {
+    if (encoding !== 'buffer') {
+      return Buffer.byteLength(chunk, 'utf8');
+    }
+    return chunk.length;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.count += this.getBinarySize(chunk, encoding);
+    if ( this.count > this.limit) {
+      return callback(new LimitExceededError());
+    }
+    callback(null, chunk);
   }
 }
 
