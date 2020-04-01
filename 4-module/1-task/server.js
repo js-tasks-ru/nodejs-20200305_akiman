645ba379e13d83bf2430cace9b15cf881b7eb54b
AKIMOV Aleksey <aleksey.akimov@vaz.ru>
2020-04-01 16:01:11 +0400

    решение задач 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,16 +1,24 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs'); // подключаем модуль работы с файлом для отдачи
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  const pathname = url.parse(req.url).pathname.slice(1); // путь до файла из ссылки
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const stream = fs.createReadStream(filepath);
+
   switch (req.method) {
     case 'GET':
+      // если в пути к файлу есть '/' (то есть вложенные директории) - пробрасывать 400 статус
+      if (~pathname.search('/')) {
+        res.statusCode = 400;
+        res.end('400');
+      }
 
       break;
 
@@ -18,6 +26,28 @@ server.on('request', (req, res) => {
       res.statusCode = 501;
       res.end('Not implemented');
   }
+
+  stream.on('data', (chunk) => {
+    res.write(chunk);
+  });
+
+  // при успешном чтении - выводить
+  stream.on('end', () => {
+    res.statusCode = 200;
+    res.end();
+  });
+
+  // при ошибке чтения файла
+  stream.on('error', () => {
+    res.statusCode = 404;
+    res.end();
+  });
+
+  // ошибки на все остальные любые случаи
+  req.on('error', (err) => {
+    res.statusCode = 500;
+    res.end('500');
+  })
 });
 
 module.exports = server;
