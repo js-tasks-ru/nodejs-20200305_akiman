b22fb1f0c6f5ba5e8ac8aef97eef10d7b313d0c2
AKIMOV Aleksey <aleksey.akimov@vaz.ru>
2020-04-04 09:29:18 +0400

    решение задач 6-module 1-task

--- 6-module/1-task/models/Product.js
+++ 6-module/1-task/models/Product.js
@@ -2,6 +2,29 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: mongoose.ObjectId,
+        ref: 'categorySchema',
+        required: true,
+    },
+    subcategory: {
+        type: mongoose.ObjectId,
+        ref: 'subCategorySchema',
+        required: true,
+    },
+    images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
